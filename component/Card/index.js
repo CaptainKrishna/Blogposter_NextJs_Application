6fb84d58c26bf772a9f9ca2e006c4161f9c4b18c
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-12-19 23:56:53 +0530

    second Commit

--- component/Card/index.js
+++ component/Card/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import style from './style.module.css'
 import blog from '../../config/config'
 import Image from 'next/image'
+import Link from 'next/link'
 function index() {
-    console.log(blog)
+    // console.log(blog)
     return (
         <div>
             <div className={style.Maincontainer}>
                 <div className={style.cardcontainer}>
                     {blog.map((e) => (
+                        <Link  href={`/Card/${e.slug}`}>
                         <div key={e.key} className={style.card}>
                             <div className={style.imagecontainer}>
                                 <img src={e.img} width={300} />
@@ -18,6 +20,7 @@ function index() {
                                 <p>{e.description}</p>
                             </div>
                         </div>
+                        </Link>
                     ))}
                 </div>
             </div>
---
+++ component/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './style.module.css'
+
+function index() {
+  return (
+    <div className={style.footer_container}>
+      
+
+    </div>
+  )
+}
+
+export default index
